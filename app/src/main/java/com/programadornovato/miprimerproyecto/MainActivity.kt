75c2284bcb861abc1659fac85f4b4c23542aeaaf
Programador Novato <info@programadornovato.com>
2021-01-26 18:17:15 -0600

    Debuguear nuestra aplicación en Android Studio [05]
    Video:
    https://youtu.be/aONKohLi0B0

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -6,33 +6,18 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("onCreate")
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        println("onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("onDestroy")
+        var calificacion=8
+        println("La calificacion del alumno es: $calificacion")
+        if(calificacion>=0 && calificacion<6){
+            println("El estudiante se ch1ng0")
+        }else if(calificacion>=6 && calificacion<=9){
+            println("El alumno aprobo")
+        }else if(calificacion==10){
+            println("El alumno es un 1ng0n")
+        }else{
+            println("Esta calificacion no es valida")
+        }
     }
 
 }

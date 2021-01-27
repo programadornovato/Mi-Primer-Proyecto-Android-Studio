dfd21af399f5945a8746970f11f07f1a9cc3f29c
Programador Novato <info@programadornovato.com>
2021-01-26 20:17:39 -0600

    Toast (avisos) en Android Studio [06]
    Video:
    https://youtu.be/wVvC86SrwEA

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -2,22 +2,13 @@ package com.programadornovato.miprimerproyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var calificacion=8
-        println("La calificacion del alumno es: $calificacion")
-        if(calificacion>=0 && calificacion<6){
-            println("El estudiante se ch1ng0")
-        }else if(calificacion>=6 && calificacion<=9){
-            println("El alumno aprobo")
-        }else if(calificacion==10){
-            println("El alumno es un 1ng0n")
-        }else{
-            println("Esta calificacion no es valida")
-        }
+        Toast.makeText(this,"Hola y adios humano",Toast.LENGTH_LONG).show()
     }
 
 }

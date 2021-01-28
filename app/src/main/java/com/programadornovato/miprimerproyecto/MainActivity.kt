0dde8307a9b829a18279144ae227db435f212f28
Programador Novato <info@programadornovato.com>
2021-01-28 15:01:00 -0600

    ¿Quieres ser mi novi@ con Android Studio(Kotlin) ♥️[10]
    Video:
    https://youtu.be/HyaDMoaB5mk

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -2,40 +2,40 @@ package com.programadornovato.miprimerproyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var tp:EditText?=null
-    var tvr:TextView?=null
+    private var bn:Button?=null
+    private var bs:Button?=null
+    private var tvr:TextView?=null
+    val displayMetrics=DisplayMetrics()
+    var alto=0
+    var ancho=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tp=findViewById(R.id.txtPalindromo)
-        tvr=findViewById(R.id.txtViewResultado)
+        bn=findViewById(R.id.btnNo)
+        bs=findViewById(R.id.btnSi)
+        tvr=findViewById(R.id.txtViewRespuesta)
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        alto=displayMetrics.heightPixels
+        ancho=displayMetrics.widthPixels
+    }
+    fun clickNo(view:View){
+        var randomX=(100..ancho-100).random().toFloat()
+        var randomY=(100..alto-100).random().toFloat()
+        bn?.x=randomX
+        bn?.y=randomY
+    }
+    fun clickSi(view:View){
+        tvr?.text="Sabia que caerias en mis brazos"
     }
 
-    fun validaPalindromo(viwe:View){
-        var texto=tp?.text.toString()
-        texto=texto.toLowerCase()
-        texto=texto.replace("\\s".toRegex(),"")
-        val longitud=texto.length
-        var igual=true
-        var cont=0
-        for (i in longitud-1 downTo 0){
-            if(texto[i]!=texto[cont]){
-                igual=false
-                break
-            }
-            cont++
-        }
-        if(igual==true){
-            tvr?.text="Si humano si es un palindromo"
-        }else{
-            tvr?.text="Humano estupido no es palindromo"
-        }
-    }
 
 }

cace6a3f792f8516174c9c21dcdf806f24aad452
Programador Novato <info@programadornovato.com>
2021-01-29 20:35:34 -0600

    Ejercicio.- Menú bancario en Android Studio 3ra parte [15]
    Video: https://youtu.be/vzsE1eoBuKM

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
+import android.widget.Toast
 import java.math.BigDecimal
 import kotlin.math.PI
 import kotlin.math.round
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     private var txtIngresar:EditText?=null
     private var txtRetirar:EditText?=null
+
+    private var saldo=100.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         txtRetirar?.visibility=View.INVISIBLE
         if(rbVerSaldo?.isChecked()==true){
             tvVerSaldo?.visibility=View.VISIBLE
-            tvVerSaldo?.text="Humano tu saldo es de "
+            tvVerSaldo?.text="Humano tu saldo es de ${saldo}"
         }
         if(rbIngresar?.isChecked()==true){
             txtIngresar?.visibility=View.VISIBLE
@@ -52,5 +55,39 @@ class MainActivity : AppCompatActivity() {
             tvVerSaldo?.text="Adios humano"
         }
     }
+    fun btnOK(view:View){
+        tvVerSaldo?.visibility=View.INVISIBLE
+        txtIngresar?.visibility=View.INVISIBLE
+        txtRetirar?.visibility=View.INVISIBLE
+        if(rbVerSaldo?.isChecked()==true){
+            tvVerSaldo?.text="Humano tu saldo es de $saldo"
+        }
+        if(rbIngresar?.isChecked()==true){
+            var ingresar=txtIngresar?.text.toString().toDouble()
+            saldo=saldo+ingresar
+            tvVerSaldo?.text="Humano tu saldo es de $saldo"
+            tvVerSaldo?.visibility=View.VISIBLE
+            Toast.makeText(this,"Humano tu operacion se realizo de forma exitos",Toast.LENGTH_LONG).show()
+        }
+        if(rbRetirar?.isChecked()==true){
+            var retirar=txtRetirar?.text.toString().toDouble()
+            if(saldo-retirar < 0.0){
+                tvVerSaldo?.visibility=View.VISIBLE
+                tvVerSaldo?.text="Humano pillin no tienes tanto dinero"
+            }else{
+                saldo=saldo-retirar
+                tvVerSaldo?.text="Humano tu saldo es de $saldo"
+                tvVerSaldo?.visibility=View.VISIBLE
+                Toast.makeText(this,"Humano tu operacion se realizo de forma exitos",Toast.LENGTH_LONG).show()
+            }
 
+        }
+        if(rbSalir?.isChecked()==true){
+            tvVerSaldo?.visibility=View.VISIBLE
+            tvVerSaldo?.text="Adios humano"
+            finish()
+            System.exit(0)
+        }
+
+    }
 }

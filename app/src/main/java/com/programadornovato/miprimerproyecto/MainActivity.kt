76f6376b93d6fbc38068118cc5c80857582be406
Programador Novato <info@programadornovato.com>
2021-02-02 16:13:25 -0600

    ListView en Android Studio [22]
    Video:
    https://youtu.be/CCBno9jX3jI

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -11,41 +11,31 @@ import kotlin.math.PI
 import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
-    private var chKotlin:CheckBox?=null
-    private var chJava:CheckBox?=null
-    private var chC:CheckBox?=null
-    private var chPHP:CheckBox?=null
+    private var lvLenguajes:ListView?=null
     private var tvSeleccion:TextView?=null
+
+    private val lenguajes = arrayOf("Kotlin","Java","C++","PHP","Javascript","C#","VB.Net")
+    private val ranking = arrayOf( "1"     ,"5"   ,"2"  ,"6"  ,"2"         ,"3" ,"4")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        chKotlin=findViewById(R.id.chKotlin)
-        chJava=findViewById(R.id.chJava)
-        chC=findViewById(R.id.chC)
-        chPHP=findViewById(R.id.chPHP)
+
+        lvLenguajes=findViewById(R.id.lvLenguajes)
         tvSeleccion=findViewById(R.id.tvSeleccion)
 
-    }
-    fun accion(view: View){
-        var seleccion=""
-        if(chKotlin?.isChecked==true){
-            seleccion=seleccion+"Usted selecciono ${chKotlin?.text}\n"
-        }
-        if(chJava?.isChecked==true){
-            seleccion=seleccion+"Usted selecciono ${chJava?.text}\n"
-        }
-        if(chC?.isChecked==true){
-            seleccion=seleccion+"Usted selecciono ${chC?.text}\n"
-        }
-        if(chPHP?.isChecked==true){
-            seleccion=seleccion+"Usted selecciono ${chPHP?.text}\n"
-        }
-        if(seleccion.isEmpty()==true){
-            tvSeleccion?.text="Humano seleccionar un lenguaje!!!!"
-        }else{
-            tvSeleccion?.text=seleccion
-        }
+        var adaptador:ArrayAdapter<String> = ArrayAdapter<String>(this,R.layout.list_view_programadornovato,lenguajes)
+        lvLenguajes?.adapter=adaptador
 
+        lvLenguajes?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                tvSeleccion?.text="El ranking del lenguaje ${lvLenguajes?.getItemAtPosition(position)} es ${ranking[position]}"
+            }
+
+        }
     }
 
 }

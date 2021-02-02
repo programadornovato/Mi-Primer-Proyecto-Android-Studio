f9716c8b53c4b2a1012aa061b2b4ba52411261a3
Programador Novato <info@programadornovato.com>
2021-02-02 17:33:48 -0600

    Botones con imagenes (ImageButton) en Android Studio [23]
    Video:
    https://youtu.be/IEXBwmpCv8A

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -11,31 +11,17 @@ import kotlin.math.PI
 import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
-    private var lvLenguajes:ListView?=null
-    private var tvSeleccion:TextView?=null
-
-    private val lenguajes = arrayOf("Kotlin","Java","C++","PHP","Javascript","C#","VB.Net")
-    private val ranking = arrayOf( "1"     ,"5"   ,"2"  ,"6"  ,"2"         ,"3" ,"4")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        lvLenguajes=findViewById(R.id.lvLenguajes)
-        tvSeleccion=findViewById(R.id.tvSeleccion)
-
-        var adaptador:ArrayAdapter<String> = ArrayAdapter<String>(this,R.layout.list_view_programadornovato,lenguajes)
-        lvLenguajes?.adapter=adaptador
-
-        lvLenguajes?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                tvSeleccion?.text="El ranking del lenguaje ${lvLenguajes?.getItemAtPosition(position)} es ${ranking[position]}"
-            }
-
+    }
+    fun saludo(view: View){
+        var texto=view.contentDescription.toString()
+        if(texto=="Hola"){
+            Toast.makeText(this,"¡¡Hola humano como estas!!",Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(this,"¡¡Adios humano te voy a extrañar!!",Toast.LENGTH_LONG).show()
         }
     }
-
 }

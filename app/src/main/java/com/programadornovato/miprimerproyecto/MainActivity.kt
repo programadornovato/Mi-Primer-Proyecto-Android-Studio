e0d6e53719b2738caa1ce4b7d83488ad9836c463
Programador Novato <info@programadornovato.com>
2021-02-04 03:03:56 -0600

    Cargar una pagina web con WebView en Android Studio [26]
    Video:
    https://youtu.be/VUn9p7TBVsU

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import java.math.BigDecimal
@@ -12,18 +13,20 @@ import kotlin.math.PI
 import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
-    private var txtDato:EditText?=null
+    private var txtURL:EditText?=null
+    private var navegador:WebView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtDato=findViewById(R.id.txtDato)
+        txtURL=findViewById(R.id.txtURL)
+        navegador=findViewById(R.id.navegador)
 
     }
-    fun envia(view:View){
-        var dato=txtDato?.text.toString()
-        var intento=Intent(this,ActivityRecibe::class.java)
-        intento.putExtra("dato",dato)
-        startActivity(intento)
+    fun cargar(view: View){
+        var url=txtURL?.text.toString()
+        navegador?.clearCache(false)
+        navegador?.settings?.javaScriptEnabled=true
+        navegador?.loadUrl(url)
     }
 }

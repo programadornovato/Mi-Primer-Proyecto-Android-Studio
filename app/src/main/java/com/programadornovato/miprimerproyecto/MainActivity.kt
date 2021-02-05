0a8b69b36cfe7f0ecce58d8c4ee9574cf5cce9f6
Programador Novato <info@programadornovato.com>
2021-02-05 05:52:42 -0600

    Crear un cronometro de cuenta regresiva 2da parte en Android Studio [30]
    Video:
    https://youtu.be/qVeG7w9EILo

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,33 +1,31 @@
 package com.programadornovato.miprimerproyecto
 
-import android.content.DialogInterface
-import android.content.Intent
-import android.graphics.Bitmap
 import android.media.RingtoneManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.webkit.*
 import android.widget.*
-import androidx.appcompat.app.AlertDialog
-import java.math.BigDecimal
-import kotlin.math.PI
-import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
-    private var txtTiempo:EditText?=null
+    private var txtSegundos:EditText?=null
+    private var txtMinutos:EditText?=null
+    private var txtHoras:EditText?=null
     private var tvCuentaAtras:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtTiempo=findViewById(R.id.txtTiempo)
+        txtSegundos=findViewById(R.id.txtSegundos)
+        txtMinutos=findViewById(R.id.txtMinutos)
+        txtHoras=findViewById(R.id.txtHoras)
         tvCuentaAtras=findViewById(R.id.tvCuentaAtras)
     }
     fun play(view: View){
-        var tiempoSegundos=txtTiempo?.text.toString().toLong()
-        var tiempoMilisegundos=tiempoSegundos*1000
+        val seg=txtSegundos?.text.toString().toLong()*1000
+        val min=txtMinutos?.text.toString().toLong()*60*1000
+        val hor=txtHoras?.text.toString().toLong()*60*60*1000
+        var tiempoMilisegundos=seg+min+hor
         object : CountDownTimer(tiempoMilisegundos,1000){
             override fun onFinish() {
                 val notificacion=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
@@ -37,8 +35,12 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTick(millisUntilFinished: Long) {
-                val tiempoSegundos=(millisUntilFinished/1000).toInt()+1
-                tvCuentaAtras?.text=tiempoSegundos.toString().padStart(2,'0')
+                var tiempoSegundos=(millisUntilFinished/1000).toInt()+1
+                val horas=tiempoSegundos/3600
+                tiempoSegundos=tiempoSegundos%3600
+                val min=tiempoSegundos/60
+                tiempoSegundos=tiempoSegundos%60
+                tvCuentaAtras?.text=horas.toString().padStart(2,'0')+":"+min.toString().padStart(2,'0')+":"+tiempoSegundos.toString().padStart(2,'0')
             }
         }.start()
     }

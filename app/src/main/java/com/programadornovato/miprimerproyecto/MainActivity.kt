84d0c7767e8e883e21a27746629d9db544d01c47
Programador Novato <info@programadornovato.com>
2021-02-18 15:53:56 -0600

    Múltiples registros con SharedPreferences en Android Studio [34]
    Video:
    https://youtu.be/nJeF9LxnjU0

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.programadornovato.miprimerproyecto
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.media.RingtoneManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -11,28 +12,46 @@ import android.widget.*
 import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    var txtIdBuscar:EditText?=null
+    var txtId:EditText?=null
     var txtNombre:EditText?=null
-    var txtApellido:EditText?=null
+    var txtDireccion:EditText?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        txtIdBuscar=findViewById(R.id.txtIdBuscar)
+        txtId=findViewById(R.id.txtId)
         txtNombre=findViewById(R.id.txtNombre)
-        txtApellido=findViewById(R.id.txtApellido)
-
-        var pref=getSharedPreferences("datos_persona",Context.MODE_PRIVATE)
+        txtDireccion=findViewById(R.id.txtDireccion)
+    }
+    fun buscar(view: View){
+        var buscarId=txtIdBuscar?.text.toString()
+        var pref=getSharedPreferences(buscarId,Context.MODE_PRIVATE)
+        var id=pref.getString("id","")
         var nombre=pref.getString("nombre","")
-        txtNombre?.setText(nombre)
-        var apellido=pref.getString("apellido","")
-        txtApellido?.setText(apellido)
+        var direccion=pref.getString("direccion","")
+        if(id!!.length>0){
+            Toast.makeText(this,"Se ha encontrado el registro",Toast.LENGTH_LONG).show()
+            txtId?.setText(id)
+            txtNombre?.setText(nombre)
+            txtDireccion?.setText(direccion)
+        }else{
+            Toast.makeText(this,"No se ha encontrado el registro",Toast.LENGTH_LONG).show()
+        }
 
     }
     fun guardar(view: View){
-        var pref=getSharedPreferences("datos_persona",Context.MODE_PRIVATE)
+        var pref=getSharedPreferences(txtId?.text.toString(),Context.MODE_PRIVATE)
         var editor=pref.edit()
+        editor.putString("id",txtId?.text.toString())
         editor.putString("nombre",txtNombre?.text.toString())
-        editor.putString("apellido",txtApellido?.text.toString())
+        editor.putString("direccion",txtDireccion?.text.toString())
         editor.commit()
-        Toast.makeText(this,"Se ha guardado exitosamente",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"Registro guardado exitosamente",Toast.LENGTH_LONG).show()
+        txtId?.setText("")
+        txtNombre?.setText("")
+        txtDireccion?.setText("")
     }
 }

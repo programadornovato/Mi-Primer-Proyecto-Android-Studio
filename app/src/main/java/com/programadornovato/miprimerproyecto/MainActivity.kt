dbae9fc40780098a330897b86728b503c88146c1
Programador Novato <info@programadornovato.com>
2021-02-17 19:17:18 -0600

    DatePicker en Android Studio [31]
    Video:
    https://youtu.be/WJCy_1RDFbg

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,47 +1,42 @@
 package com.programadornovato.miprimerproyecto
 
 import android.media.RingtoneManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
-    private var txtSegundos:EditText?=null
-    private var txtMinutos:EditText?=null
-    private var txtHoras:EditText?=null
-    private var tvCuentaAtras:TextView?=null
+    var txtFecha:EditText?=null
+    var btnFecha:ImageButton?=null
+    var dpFecha:DatePicker?=null
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtSegundos=findViewById(R.id.txtSegundos)
-        txtMinutos=findViewById(R.id.txtMinutos)
-        txtHoras=findViewById(R.id.txtHoras)
-        tvCuentaAtras=findViewById(R.id.tvCuentaAtras)
-    }
-    fun play(view: View){
-        val seg=txtSegundos?.text.toString().toLong()*1000
-        val min=txtMinutos?.text.toString().toLong()*60*1000
-        val hor=txtHoras?.text.toString().toLong()*60*60*1000
-        var tiempoMilisegundos=seg+min+hor
-        object : CountDownTimer(tiempoMilisegundos,1000){
-            override fun onFinish() {
-                val notificacion=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
-                val r=RingtoneManager.getRingtone(this@MainActivity,notificacion)
-                r.play()
-                this.cancel()
-            }
+        txtFecha=findViewById(R.id.txtFecha)
+        btnFecha=findViewById(R.id.btnFecha)
+        dpFecha=findViewById(R.id.dpFecha)
 
-            override fun onTick(millisUntilFinished: Long) {
-                var tiempoSegundos=(millisUntilFinished/1000).toInt()+1
-                val horas=tiempoSegundos/3600
-                tiempoSegundos=tiempoSegundos%3600
-                val min=tiempoSegundos/60
-                tiempoSegundos=tiempoSegundos%60
-                tvCuentaAtras?.text=horas.toString().padStart(2,'0')+":"+min.toString().padStart(2,'0')+":"+tiempoSegundos.toString().padStart(2,'0')
-            }
-        }.start()
+        txtFecha?.setText(getFechaDtePicker())
+
+        dpFecha?.setOnDateChangedListener{
+            dpFecha,anio,mes,dia->
+            txtFecha?.setText(getFechaDtePicker())
+            dpFecha?.visibility=View.GONE
+        }
+    }
+    fun getFechaDtePicker():String{
+        var dia=dpFecha?.dayOfMonth.toString().padStart(2,'0')
+        var mes=(dpFecha!!.month+1).toString().padStart(2,'0')
+        var anio=dpFecha?.year.toString().padStart(4,'0')
+        return dia+"/"+mes+"/"+anio
+    }
+    fun muestraCalendario(view: View){
+        dpFecha?.visibility=View.VISIBLE
     }
 }

081bb6a97c210ca5c08a680be518ef2cfeeb49c1
Programador Novato <info@programadornovato.com>
2021-02-23 18:08:41 -0600

    LinerLayout horizontal y vertical en Android Studio [38]
    Video:
    https://youtu.be/ohdUhVeF03o

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -9,51 +9,24 @@ import android.widget.*
 import java.io.*
 
 class MainActivity : AppCompatActivity() {
-    var txtNombre:EditText?=null
-    var txtContenido:EditText?=null
+    var llLogin:LinearLayout?=null
+    var llRegistro:LinearLayout?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        txtNombre=findViewById(R.id.txtNombre)
-        txtContenido=findViewById(R.id.txtContenido)
+        llLogin=findViewById(R.id.llLogin)
+        llRegistro=findViewById(R.id.llRegistro)
     }
-    fun guardar(view: View){
-        val nombre=txtNombre?.text.toString()
-        val contenido=txtContenido?.text.toString()
-        try {
-            val sd=Environment.getExternalStorageDirectory()
-            val rutaArchivo= File(sd.path.toString(),nombre)
-            val archivo=OutputStreamWriter(openFileOutput(nombre,Activity.MODE_PRIVATE))
-            archivo.write(contenido)
-            archivo.flush()
-            archivo.close()
-            Toast.makeText(this,"El archivo se guardo exitosamente",Toast.LENGTH_LONG).show()
-            txtNombre?.setText("")
-            txtContenido?.setText("")
-        }catch (e:IOException){
-            Toast.makeText(this,"Hubo un error al guardar "+e,Toast.LENGTH_LONG).show()
+    fun clickLoginRegistro(view: View){
+        val nombreCompleto=resources.getResourceName(view.id)
+        println(nombreCompleto)
+        val nombre=nombreCompleto.substring(nombreCompleto.lastIndexOf("/")+1)
+        if(nombre=="rdLogin"){
+            llLogin?.visibility=View.VISIBLE
+            llRegistro?.visibility=View.GONE
+        }else if(nombre=="rdRegistro"){
+            llLogin?.visibility=View.GONE
+            llRegistro?.visibility=View.VISIBLE
         }
-    }
-    fun buscar(view: View){
-        val nombre=txtNombre?.text.toString()
-        var contenido=""
-        try {
-        val sd=Environment.getExternalStorageDirectory()
-        val rutaArchivo= File(sd.path.toString(),nombre)
-        val archivo=InputStreamReader(openFileInput(nombre))
-        val bf=BufferedReader(archivo)
-        var linea=bf.readLine()
-        while (linea!=null){
-            contenido=contenido+linea+"\n"
-            linea=bf?.readLine()
-        }
-        archivo.close()
-        bf.close()
-        txtContenido?.setText(contenido)
-        }catch (e:IOException){
-            Toast.makeText(this,"Hubo un error al guardar "+e,Toast.LENGTH_LONG).show()
-        }
-
     }
 }

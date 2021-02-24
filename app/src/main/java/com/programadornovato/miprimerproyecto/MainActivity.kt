a7e700fbe4e0f3dbf274b7ef8189fe751c4f88b1
Programador Novato <info@programadornovato.com>
2021-02-24 05:56:33 -0600

    TableLayout en Android Studio [39]
    Video:
    https://youtu.be/axe6AaCZCpw

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -9,24 +9,8 @@ import android.widget.*
 import java.io.*
 
 class MainActivity : AppCompatActivity() {
-    var llLogin:LinearLayout?=null
-    var llRegistro:LinearLayout?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        llLogin=findViewById(R.id.llLogin)
-        llRegistro=findViewById(R.id.llRegistro)
-    }
-    fun clickLoginRegistro(view: View){
-        val nombreCompleto=resources.getResourceName(view.id)
-        println(nombreCompleto)
-        val nombre=nombreCompleto.substring(nombreCompleto.lastIndexOf("/")+1)
-        if(nombre=="rdLogin"){
-            llLogin?.visibility=View.VISIBLE
-            llRegistro?.visibility=View.GONE
-        }else if(nombre=="rdRegistro"){
-            llLogin?.visibility=View.GONE
-            llRegistro?.visibility=View.VISIBLE
-        }
     }
 }

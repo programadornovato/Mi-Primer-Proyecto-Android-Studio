e814156c0f4d9fa3c3562d4ed5bd67549660cbdc
Programador Novato <info@programadornovato.com>
2021-02-25 03:19:34 -0600

    Llenar una tabla de forma dinámica (TableLayout) Android Studio [41]
    Video:
    https://youtu.be/N_s8iR0eY8g

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,116 +1,35 @@
 package com.programadornovato.miprimerproyecto
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.TableLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var tvRes:TextView?=null
+    var matriz = arrayOf(
+            arrayOf(0,1,2),
+            arrayOf(3,4,5),
+            arrayOf(6,7,8)
+    )
+    var tlTabla:TableLayout?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvRes=findViewById(R.id.tvRes)
-    }
-    fun calcular(view: View){
-        val boton=view as Button
-        val textoBoton=boton.text.toString()
-        var concatenar=tvRes?.text.toString()+textoBoton
-        var mostrar=removerCerosIzquirda(concatenar)
-        if(textoBoton=="="){
-            var resultado=0.0
-            try {
-                resultado=eval(tvRes?.text.toString())
-                tvRes?.text=resultado.toString()
-            }catch (e:Exception){
-                println(e)
-            }
-        }else if(textoBoton=="RESET"){
-            tvRes?.text="0"
-        }else{
-            tvRes?.text=mostrar
+        tlTabla=findViewById(R.id.tlTabla)
+        tlTabla?.removeAllViews()
+        for (i in (0 until matriz.count())){
+            val registro = LayoutInflater.from(this).inflate(R.layout.row_table_pn,null,false)
+            val tv0=registro.findViewById<View>(R.id.tv0) as TextView
+            val tv1=registro.findViewById<View>(R.id.tv1) as TextView
+            val tv2=registro.findViewById<View>(R.id.tv2) as TextView
+            tv0.text=matriz[i][0].toString()
+            tv1.text=matriz[i][1].toString()
+            tv2.text=matriz[i][2].toString()
+            tlTabla?.addView(registro)
         }
-
-    }
-    fun removerCerosIzquirda(str:String):String?{
-        var i=0
-        while (i<str.length && str[i]=='0')i++
-        val sb = StringBuffer(str)
-        sb.replace(0,i,"")
-        return sb.toString()
-    }
-
-    fun eval(str: String): Double {
-        return object : Any() {
-            var pos = -1
-            var ch = 0
-            fun nextChar() {
-                ch = if (++pos < str.length) str[pos].toInt() else -1
-            }
-
-            fun eat(charToEat: Int): Boolean {
-                while (ch == ' '.toInt()) nextChar()
-                if (ch == charToEat) {
-                    nextChar()
-                    return true
-                }
-                return false
-            }
-
-            fun parse(): Double {
-                nextChar()
-                val x = parseExpression()
-                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
-                return x
-            }
-
-            // Grammar:
-            // expression = term | expression `+` term | expression `-` term
-            // term = factor | term `*` factor | term `/` factor
-            // factor = `+` factor | `-` factor | `(` expression `)`
-            //        | number | functionName factor | factor `^` factor
-            fun parseExpression(): Double {
-                var x = parseTerm()
-                while (true) {
-                    if (eat('+'.toInt())) x += parseTerm() // addition
-                    else if (eat('-'.toInt())) x -= parseTerm() // subtraction
-                    else return x
-                }
-            }
-
-            fun parseTerm(): Double {
-                var x = parseFactor()
-                while (true) {
-                    if (eat('*'.toInt())) x *= parseFactor() // multiplication
-                    else if (eat('/'.toInt())) x /= parseFactor() // division
-                    else return x
-                }
-            }
-
-            fun parseFactor(): Double {
-                if (eat('+'.toInt())) return parseFactor() // unary plus
-                if (eat('-'.toInt())) return -parseFactor() // unary minus
-                var x: Double
-                val startPos = pos
-                if (eat('('.toInt())) { // parentheses
-                    x = parseExpression()
-                    eat(')'.toInt())
-                } else if (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) { // numbers
-                    while (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) nextChar()
-                    x = str.substring(startPos, pos).toDouble()
-                } else if (ch >= 'a'.toInt() && ch <= 'z'.toInt()) { // functions
-                    while (ch >= 'a'.toInt() && ch <= 'z'.toInt()) nextChar()
-                    val func = str.substring(startPos, pos)
-                    x = parseFactor()
-                    x = if (func == "sqrt") Math.sqrt(x) else if (func == "sin") Math.sin(Math.toRadians(x)) else if (func == "cos") Math.cos(Math.toRadians(x)) else if (func == "tan") Math.tan(Math.toRadians(x)) else throw RuntimeException("Unknown function: $func")
-                } else {
-                    throw RuntimeException("Unexpected: " + ch.toChar())
-                }
-                if (eat('^'.toInt())) x = Math.pow(x, parseFactor()) // exponentiation
-                return x
-            }
-        }.parse()
     }
 }

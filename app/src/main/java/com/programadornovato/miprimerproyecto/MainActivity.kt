da216c73a4329341c53e685003dadc43afaffce9
Programador Novato <info@programadornovato.com>
2021-02-26 04:02:43 -0600

    Tabla con ScrollView en Android Studio [44]
    Video:
    https://youtu.be/ArvZbPFjanI

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -11,23 +11,21 @@ import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    var tlTabla:TableLayout?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun clicBoton(view: View){
-        when(view.id){
-            R.id.btn0 -> Toast.makeText(this,"Diste click en 0",Toast.LENGTH_LONG).show()
-            R.id.btn1 -> Toast.makeText(this,"Diste click en 1",Toast.LENGTH_LONG).show()
-            R.id.btn2 -> Toast.makeText(this,"Diste click en 2",Toast.LENGTH_LONG).show()
-            R.id.btn3 -> Toast.makeText(this,"Diste click en 3",Toast.LENGTH_LONG).show()
-            R.id.btn4 -> Toast.makeText(this,"Diste click en 4",Toast.LENGTH_LONG).show()
-            R.id.btn5 -> Toast.makeText(this,"Diste click en 5",Toast.LENGTH_LONG).show()
-            R.id.btn6 -> Toast.makeText(this,"Diste click en 6",Toast.LENGTH_LONG).show()
-            R.id.btn7 -> Toast.makeText(this,"Diste click en 7",Toast.LENGTH_LONG).show()
-            R.id.btn8 -> Toast.makeText(this,"Diste click en 8",Toast.LENGTH_LONG).show()
-            R.id.btn9 -> Toast.makeText(this,"Diste click en 9",Toast.LENGTH_LONG).show()
-            R.id.btn10 -> Toast.makeText(this,"Diste click en 10",Toast.LENGTH_LONG).show()
+        tlTabla=findViewById(R.id.tlTabla)
+
+        for (i in 0 .. 35){
+            val registro=LayoutInflater.from(this).inflate(R.layout.row_table_pn,null,false)
+            val tv0=registro.findViewById<View>(R.id.tv0) as TextView
+            val tv1=registro.findViewById<View>(R.id.tv1) as TextView
+            val tv2=registro.findViewById<View>(R.id.tv2) as TextView
+            tv0?.text= ((i+1)*1).toString()
+            tv1?.text= ((i+1)*2).toString()
+            tv2?.text= ((i+1)*3).toString()
+            tlTabla?.addView(registro)
         }
     }
 }

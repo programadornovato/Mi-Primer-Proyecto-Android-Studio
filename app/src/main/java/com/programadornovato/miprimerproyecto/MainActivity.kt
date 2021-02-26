d41cdf83304a70499a9f1fe9c78f4cfd16e5a9b3
Programador Novato <info@programadornovato.com>
2021-02-25 18:18:11 -0600

    FrameLayout Android Studio [42]
    Video:
    https://youtu.be/4MllykEmI8Q

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -10,26 +10,20 @@ import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var matriz = arrayOf(
-            arrayOf(0,1,2),
-            arrayOf(3,4,5),
-            arrayOf(6,7,8)
-    )
-    var tlTabla:TableLayout?=null
+    var btnPerro:Button?=null
+    var btnGato:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tlTabla=findViewById(R.id.tlTabla)
-        tlTabla?.removeAllViews()
-        for (i in (0 until matriz.count())){
-            val registro = LayoutInflater.from(this).inflate(R.layout.row_table_pn,null,false)
-            val tv0=registro.findViewById<View>(R.id.tv0) as TextView
-            val tv1=registro.findViewById<View>(R.id.tv1) as TextView
-            val tv2=registro.findViewById<View>(R.id.tv2) as TextView
-            tv0.text=matriz[i][0].toString()
-            tv1.text=matriz[i][1].toString()
-            tv2.text=matriz[i][2].toString()
-            tlTabla?.addView(registro)
-        }
+        btnPerro=findViewById(R.id.btnPerro)
+        btnGato=findViewById(R.id.btnGato)
+    }
+    fun clickPerro(view: View){
+        btnPerro?.visibility=View.GONE
+        btnGato?.visibility=View.VISIBLE
+    }
+    fun clickGato(view: View){
+        btnPerro?.visibility=View.VISIBLE
+        btnGato?.visibility=View.GONE
     }
 }

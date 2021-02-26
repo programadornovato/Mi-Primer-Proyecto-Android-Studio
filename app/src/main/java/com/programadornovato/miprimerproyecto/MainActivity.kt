5fcb3f84c50fe66516bdf87c20e993508927b959
Programador Novato <info@programadornovato.com>
2021-02-25 21:36:06 -0600

    ScrollView Android Studio [43]
    Video:
    https://youtu.be/BtuJdUTwORA

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -6,24 +6,28 @@ import android.view.View
 import android.widget.Button
 import android.widget.TableLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var btnPerro:Button?=null
-    var btnGato:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnPerro=findViewById(R.id.btnPerro)
-        btnGato=findViewById(R.id.btnGato)
     }
-    fun clickPerro(view: View){
-        btnPerro?.visibility=View.GONE
-        btnGato?.visibility=View.VISIBLE
-    }
-    fun clickGato(view: View){
-        btnPerro?.visibility=View.VISIBLE
-        btnGato?.visibility=View.GONE
+    fun clicBoton(view: View){
+        when(view.id){
+            R.id.btn0 -> Toast.makeText(this,"Diste click en 0",Toast.LENGTH_LONG).show()
+            R.id.btn1 -> Toast.makeText(this,"Diste click en 1",Toast.LENGTH_LONG).show()
+            R.id.btn2 -> Toast.makeText(this,"Diste click en 2",Toast.LENGTH_LONG).show()
+            R.id.btn3 -> Toast.makeText(this,"Diste click en 3",Toast.LENGTH_LONG).show()
+            R.id.btn4 -> Toast.makeText(this,"Diste click en 4",Toast.LENGTH_LONG).show()
+            R.id.btn5 -> Toast.makeText(this,"Diste click en 5",Toast.LENGTH_LONG).show()
+            R.id.btn6 -> Toast.makeText(this,"Diste click en 6",Toast.LENGTH_LONG).show()
+            R.id.btn7 -> Toast.makeText(this,"Diste click en 7",Toast.LENGTH_LONG).show()
+            R.id.btn8 -> Toast.makeText(this,"Diste click en 8",Toast.LENGTH_LONG).show()
+            R.id.btn9 -> Toast.makeText(this,"Diste click en 9",Toast.LENGTH_LONG).show()
+            R.id.btn10 -> Toast.makeText(this,"Diste click en 10",Toast.LENGTH_LONG).show()
+        }
     }
 }

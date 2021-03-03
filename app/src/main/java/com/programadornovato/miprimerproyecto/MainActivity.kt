66ffbe71f12d5c2722ffb0846d3908c2d45a1b6d
Programador Novato <info@programadornovato.com>
2021-03-02 20:14:16 -0600

    Reproducir audios mp3 en Android Studio [46]
    Video:
    https://youtu.be/VWSqi7qguw4

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.programadornovato.miprimerproyecto
 
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,22 +14,19 @@ import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var tlTabla:TableLayout?=null
+    var sp:SoundPool?=null
+    var sonidoReproducir=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tlTabla=findViewById(R.id.tlTabla)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setIcon(R.mipmap.ic_launcher)
-        for (i in 0 .. 35){
-            val registro=LayoutInflater.from(this).inflate(R.layout.row_table_pn,null,false)
-            val tv0=registro.findViewById<View>(R.id.tv0) as TextView
-            val tv1=registro.findViewById<View>(R.id.tv1) as TextView
-            val tv2=registro.findViewById<View>(R.id.tv2) as TextView
-            tv0?.text= ((i+1)*1).toString()
-            tv1?.text= ((i+1)*2).toString()
-            tv2?.text= ((i+1)*3).toString()
-            tlTabla?.addView(registro)
-        }
+        sp= SoundPool(1,AudioManager.STREAM_MUSIC,1)
+        sonidoReproducir=sp?.load(this,R.raw.mario_vida,1)!!
+    }
+    fun reproduceSoundPool(view:View){
+        sp?.play(sonidoReproducir,1f,1f,1,0,1f)
+    }
+    fun reproducirMediaPlayer(view: View){
+        val mp=MediaPlayer.create(this,R.raw.mario_muere)
+        mp.start()
     }
 }

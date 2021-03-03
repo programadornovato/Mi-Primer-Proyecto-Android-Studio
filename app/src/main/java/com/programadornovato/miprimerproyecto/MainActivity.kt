2127d28b8c7aaf5d38092973a3e296d36ba9bdc2
Programador Novato <info@programadornovato.com>
2021-03-03 03:29:54 -0600

    Detectar la posición de nuestro teléfono con sensores en Android Studio [47]
    Video:
    https://youtu.be/HqgxY8eCKBg

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,32 +1,62 @@
 package com.programadornovato.miprimerproyecto
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.SoundPool
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.TableLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var sp:SoundPool?=null
-    var sonidoReproducir=0
+    var txtPosicion:EditText?=null
+    var sm:SensorManager?=null
+    var sa:Sensor?=null
+    var SEL: SensorEventListener?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sp= SoundPool(1,AudioManager.STREAM_MUSIC,1)
-        sonidoReproducir=sp?.load(this,R.raw.mario_vida,1)!!
+        txtPosicion=findViewById(R.id.txtPosicion)
+
+        sm=getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sa=sm?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        if(sa==null){
+            Toast.makeText(this,"No hay sensor",Toast.LENGTH_LONG).show()
+        }
+        else{
+            SEL = object : SensorEventListener{
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                }
+                override fun onSensorChanged(event: SensorEvent?) {
+                    var x=event!!.values[0]
+                    txtPosicion?.setText(x.toString())
+                }
+
+            }
+        }
     }
-    fun reproduceSoundPool(view:View){
-        sp?.play(sonidoReproducir,1f,1f,1,0,1f)
+    fun iniciar(){
+        sm?.registerListener(SEL,sa,SensorManager.SENSOR_DELAY_NORMAL)
     }
-    fun reproducirMediaPlayer(view: View){
-        val mp=MediaPlayer.create(this,R.raw.mario_muere)
-        mp.start()
+
+    override fun onResume() {
+        iniciar()
+        super.onResume()
     }
+    fun detener(){
+        sm?.unregisterListener(SEL)
+    }
+
+    override fun onStop() {
+        detener()
+        super.onStop()
+    }
+
 }

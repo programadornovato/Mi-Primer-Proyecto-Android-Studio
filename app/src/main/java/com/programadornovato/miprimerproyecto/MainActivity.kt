e9718bf026e997ad6f4d166b5c32a52c430ceb96
Programador Novato <info@programadornovato.com>
2021-03-04 05:08:59 -0600

    Juego encontrar animal por sonido en Android Studio [49]
    Video:
    https://youtu.be/gMmkNqx0jpc

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -17,63 +17,41 @@ import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var txtPosicion:EditText?=null
-    var sm:SensorManager?=null
-    var sa:Sensor?=null
-    var SEL: SensorEventListener?=null
+    var listaAudios = arrayOfNulls<MediaPlayer>(6)
+    var listaImagenes = arrayOfNulls<ImageView>(6)
+    var audioBien: MediaPlayer?=null
+    var audioError: MediaPlayer?=null
+    var aleatorio=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtPosicion=findViewById(R.id.txtPosicion)
 
-        sm=getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sa=sm?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        if(sa==null){
-            Toast.makeText(this,"No hay sensor",Toast.LENGTH_LONG).show()
-        }
-        else{
-            var latigo=0
-            SEL = object : SensorEventListener{
-                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-                }
-                override fun onSensorChanged(event: SensorEvent?) {
-                    var x=event!!.values[0]
-                    txtPosicion?.setText(x.toString())
-                    if(x < -5 && latigo==0){
-                        latigo++
-                        window.decorView.setBackgroundColor(Color.BLUE)
-                    }else if(x > 5 && latigo==1){
-                        latigo++
-                        window.decorView.setBackgroundColor(Color.RED)
-                    }
-                    if(latigo==2){
-                        sonido()
-                        latigo=0
-                    }
-                }
+        listaAudios[0]=MediaPlayer.create(this,R.raw.a0)
+        listaAudios[1]=MediaPlayer.create(this,R.raw.a1)
+        listaAudios[2]=MediaPlayer.create(this,R.raw.a2)
+        listaAudios[3]=MediaPlayer.create(this,R.raw.a3)
+        listaAudios[4]=MediaPlayer.create(this,R.raw.a4)
+        listaAudios[5]=MediaPlayer.create(this,R.raw.a5)
 
-            }
+        listaImagenes[0]=findViewById(R.id.i0)
+        listaImagenes[1]=findViewById(R.id.i1)
+        listaImagenes[2]=findViewById(R.id.i2)
+        listaImagenes[3]=findViewById(R.id.i3)
+        listaImagenes[4]=findViewById(R.id.i4)
+        listaImagenes[5]=findViewById(R.id.i5)
+
+        audioBien=MediaPlayer.create(this,R.raw.bien)
+        audioError=MediaPlayer.create(this,R.raw.error)
+    }
+    fun emitirSonido(view: View){
+        aleatorio=(0 .. 5).random()
+        listaAudios[aleatorio]?.start()
+    }
+    fun clickFotoAnimal(view: View){
+        if(view.id == listaImagenes[aleatorio]?.id){
+            audioBien?.start()
+        }else{
+            audioError?.start()
         }
     }
-    fun sonido(){
-        val mp:MediaPlayer=MediaPlayer.create(this,R.raw.latigo)
-        mp.start()
-    }
-    fun iniciar(){
-        sm?.registerListener(SEL,sa,SensorManager.SENSOR_DELAY_NORMAL)
-    }
-
-    override fun onResume() {
-        iniciar()
-        super.onResume()
-    }
-    fun detener(){
-        sm?.unregisterListener(SEL)
-    }
-
-    override fun onStop() {
-        detener()
-        super.onStop()
-    }
-
 }

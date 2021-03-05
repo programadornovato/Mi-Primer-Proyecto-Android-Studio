abf058a5a8eed499384d12e47d3ac80fcd0658bf
Programador Novato <info@programadornovato.com>
2021-03-05 02:33:04 -0600

    Grabadora en Android Studio [50]
    Video:
    https://youtu.be/aaB9mQajJn0

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.programadornovato.miprimerproyecto
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -8,50 +10,73 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.media.SoundPool
 import android.os.Bundle
+import android.os.Environment.getExternalStorageDirectory
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    var listaAudios = arrayOfNulls<MediaPlayer>(6)
-    var listaImagenes = arrayOfNulls<ImageView>(6)
-    var audioBien: MediaPlayer?=null
-    var audioError: MediaPlayer?=null
-    var aleatorio=0
+    var grabadora:MediaRecorder?=null
+    var ruta:String?=null
+    var imgGrabar:ImageView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listaAudios[0]=MediaPlayer.create(this,R.raw.a0)
-        listaAudios[1]=MediaPlayer.create(this,R.raw.a1)
-        listaAudios[2]=MediaPlayer.create(this,R.raw.a2)
-        listaAudios[3]=MediaPlayer.create(this,R.raw.a3)
-        listaAudios[4]=MediaPlayer.create(this,R.raw.a4)
-        listaAudios[5]=MediaPlayer.create(this,R.raw.a5)
+        imgGrabar=findViewById(R.id.imgGrabar)
 
-        listaImagenes[0]=findViewById(R.id.i0)
-        listaImagenes[1]=findViewById(R.id.i1)
-        listaImagenes[2]=findViewById(R.id.i2)
-        listaImagenes[3]=findViewById(R.id.i3)
-        listaImagenes[4]=findViewById(R.id.i4)
-        listaImagenes[5]=findViewById(R.id.i5)
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO), 1000)
+        }
 
-        audioBien=MediaPlayer.create(this,R.raw.bien)
-        audioError=MediaPlayer.create(this,R.raw.error)
     }
-    fun emitirSonido(view: View){
-        aleatorio=(0 .. 5).random()
-        listaAudios[aleatorio]?.start()
-    }
-    fun clickFotoAnimal(view: View){
-        if(view.id == listaImagenes[aleatorio]?.id){
-            audioBien?.start()
+    fun grabar(view : View){
+
+        if(grabadora==null){
+            ruta=getExternalStorageDirectory().getAbsolutePath().toString()+"/grabacion.mp3"
+            grabadora=MediaRecorder()
+            grabadora?.setAudioSource(MediaRecorder.AudioSource.MIC)
+            grabadora?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            grabadora?.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+            grabadora?.setOutputFile(ruta)
+            try {
+                grabadora?.prepare()
+                grabadora?.start()
+                imgGrabar?.setBackgroundColor(Color.RED)
+                Toast.makeText(applicationContext, "Grabando...", Toast.LENGTH_SHORT).show()
+            }catch (e:IOException){
+                println(e)
+            }
+
         }else{
-            audioError?.start()
+            try {
+                grabadora?.stop()
+                grabadora?.release()
+                imgGrabar?.setBackgroundColor(Color.BLACK)
+                Toast.makeText(applicationContext, "Termino de grabar", Toast.LENGTH_SHORT).show()
+            }catch (e:IOException){
+                println(e)
+            }
         }
     }
+    fun reproducir(view: View){
+        var mediaPlayer=MediaPlayer()
+        try {
+            mediaPlayer.setDataSource(ruta)
+            mediaPlayer.prepare()
+        }catch (e:IOException){
+            println(e)
+        }
+        mediaPlayer.start()
+        Toast.makeText(applicationContext, "Reproduciendo audio", Toast.LENGTH_SHORT).show()
+    }
+
 }

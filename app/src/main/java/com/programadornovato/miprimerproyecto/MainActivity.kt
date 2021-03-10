dc26c915e02ee76a4a7b369a4d024053943af091
Programador Novato <info@programadornovato.com>
2021-03-10 08:13:35 -0600

    Gato (tic tac toe) Android Studio 2da Parte [52]
    Video:
    https://youtu.be/Xv76xQ9yVYU

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -37,6 +37,21 @@ class MainActivity : AppCompatActivity() {
     var imgTurnoX:ImageView?=null
     var imgTurnoO:ImageView?=null
     var turno="x"
+    var matrizGanadora= arrayOf(
+            intArrayOf(1,2,3),
+            intArrayOf(4,5,6),
+            intArrayOf(7,8,9),
+            intArrayOf(1,4,7),
+            intArrayOf(2,5,8),
+            intArrayOf(3,6,9),
+            intArrayOf(1,5,9),
+            intArrayOf(3,5,7)
+    )
+    var posicionesX=IntArray(5)
+    var posicionesO=IntArray(5)
+    var gandor=""
+    var contadorX=0
+    var contadorO=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -56,6 +71,89 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun clickImagen(view: View){
+        when(view.id){
+            R.id.img1->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=1
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=1
+                    contadorO++
+                }
+            }
+            R.id.img2->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=2
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=2
+                    contadorO++
+                }
+            }
+            R.id.img3->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=3
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=3
+                    contadorO++
+                }
+            }
+            R.id.img4->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=4
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=4
+                    contadorO++
+                }
+            }
+            R.id.img5->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=5
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=5
+                    contadorO++
+                }
+            }
+            R.id.img6->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=6
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=6
+                    contadorO++
+                }
+            }
+            R.id.img7->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=7
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=7
+                    contadorO++
+                }
+            }
+            R.id.img8->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=8
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=8
+                    contadorO++
+                }
+            }
+            R.id.img9->{
+                if(turno=="x"){
+                    posicionesX[contadorX]=9
+                    contadorX++
+                }else{
+                    posicionesO[contadorO]=9
+                    contadorO++
+                }
+            }
+        }
         if(turno=="x"){
             view.setBackgroundResource(R.drawable.gato_x)
             imgTurnoO?.setBackgroundColor(Color.RED)
@@ -67,6 +165,42 @@ class MainActivity : AppCompatActivity() {
             imgTurnoO?.setBackgroundColor(Color.WHITE)
             turno="x"
         }
+        for (i in 0 until contadorX){
+            var gano=true
+            var altoMatriz=matrizGanadora.size
+            for(j in 0 until  altoMatriz){
+                gano=coincideGanador(matrizGanadora[j],posicionesX)
+                if(gano==true){
+                    Toast.makeText(this,"Gano X",Toast.LENGTH_LONG).show()
+                    break
+                }
+            }
+            if(gano==true){
+                break
+            }
+        }
+        for (i in 0 until contadorO){
+            var gano=true
+            var altoMatriz=matrizGanadora.size
+            for(j in 0 until  altoMatriz){
+                gano=coincideGanador(matrizGanadora[j],posicionesO)
+                if(gano==true){
+                    Toast.makeText(this,"Gano O",Toast.LENGTH_LONG).show()
+                    break
+                }
+            }
+            if(gano==true){
+                break
+            }
+        }
         view.isEnabled=false
+    }
+    fun coincideGanador(registroGranador:IntArray,posiciones:IntArray):Boolean{
+        for(i in 0 until 3){
+            if(registroGranador[i]!=posiciones[i]){
+                return false
+            }
+        }
+        return true
     }
 }

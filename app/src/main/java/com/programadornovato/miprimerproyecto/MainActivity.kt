4728e0b9216795422a392895608723ad82ae4dda
Programador Novato <info@programadornovato.com>
2021-03-17 16:58:26 -0600

    Menú desplegable (ActionBar) en Android Studio [56]
    Video:
    https://youtu.be/qvjEnYzz4Dw

--- app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/programadornovato/miprimerproyecto/MainActivity.kt
@@ -15,6 +15,8 @@ import android.media.SoundPool
 import android.os.Bundle
 import android.os.Environment.getExternalStorageDirectory
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -28,5 +30,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_desplegable,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item1->{
+                Toast.makeText(this, "Opción 1", Toast.LENGTH_SHORT).show();
+            }
+            R.id.item2->{
+                Toast.makeText(this, "Opción 2", Toast.LENGTH_SHORT).show();
+            }
+            R.id.item3->{
+                Toast.makeText(this, "Opción 3", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
